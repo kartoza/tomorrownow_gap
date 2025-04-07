39ad340ca58f41bc975fae2815e14b20f38b9c4a
Danang Massandy <danangmassandy@gmail.com>
2025-04-07 23:59:53 +0000

    use t.io zarr when available

--- django_project/spw/generator/gap_input.py
+++ django_project/spw/generator/gap_input.py
@@ -5,10 +5,19 @@ Tomorrow Now GAP.
 .. note:: SPW Data Input using GAP.
 """
 from datetime import datetime
+import numpy as np
+import pytz
 from django.contrib.gis.geos import Point
+from django.core.cache import cache
 
-from gap.models import Dataset, DatasetAttribute, CastType, DatasetStore
-from gap.providers import TomorrowIODatasetReader, TIO_PROVIDER
+from gap.models import (
+    Dataset, DatasetAttribute, CastType,
+    DatasetStore, DataSourceFile
+)
+from gap.providers import (
+    TomorrowIODatasetReader, TIO_PROVIDER,
+    TioZarrReader
+)
 from gap.utils.reader import DatasetReaderInput
 from .input import SPWDataInput
 
@@ -37,6 +46,12 @@ class GapInput(SPWDataInput):
         'total_evapotranspiration_flux': 'LTNPET',
         'total_rainfall': 'LTNPrecip'
     }
+
+    # Cache key for checking Tio zarr
+    CACHE_KEY_TIO_ZARR = 'forecast_tio_zarr_available'
+    # Check every 10 minutes
+    CHECK_CACHE_KEY_TIO_ZARR_EXPIRY = 60 * 10  # 10 minutes
+    CACHE_KEY_TIO_ZARR_EXPIRY = 60 * 60 * 2  # 2 hours
 
     def __init__(
         self, latitude: float, longitude: float, current_date: datetime
@@ -151,8 +166,99 @@ class GapInput(SPWDataInput):
                     data[v] = val.values.get(k, '')
         return historical_dict
 
+    def _is_date_in_zarr(self, date: datetime) -> bool:
+        cached_value = cache.get(self.CACHE_KEY_TIO_ZARR)
+        if cached_value is not None:
+            return cached_value
+
+        # check from zarr and store to cache
+        result = False
+        dataset = Dataset.objects.get(
+            name='Tomorrow.io Short-term Forecast',
+            store_type=DatasetStore.ZARR
+        )
+        reader = TioZarrReader(
+            dataset,
+            [],
+            self.location_input,
+            self.start_date,
+            self.end_date
+        )
+        reader.setup_reader()
+        zarr_file = DataSourceFile.objects.filter(
+            dataset=dataset,
+            format=DatasetStore.ZARR,
+            is_latest=True
+        ).order_by('id').last()
+        if zarr_file is not None:
+            ds = reader.open_dataset(zarr_file)
+            existing_dates = ds['forecast_date'].values
+            np_date = np.datetime64(f'{date.date().isoformat()}')
+            result = np_date in existing_dates
+            ds.close()
+
+        # store result to cache
+        cache.set(
+            self.CACHE_KEY_TIO_ZARR,
+            result,
+            self.CACHE_KEY_TIO_ZARR_EXPIRY if result else
+            self.CHECK_CACHE_KEY_TIO_ZARR_EXPIRY
+        )
+        return result
+
+    def _read_forecast_from_zarr(self) -> dict:
+        """Read forecast data from T.io zarr."""
+        dataset = Dataset.objects.get(
+            name='Tomorrow.io Short-term Forecast',
+            store_type=DatasetStore.ZARR
+        )
+        attributes = DatasetAttribute.objects.filter(
+            attribute__variable_name__in=self.ATTRIBUTES,
+            dataset=dataset
+        )
+        reader = TioZarrReader(
+            dataset,
+            list(attributes),
+            self.location_input,
+            self.start_date,
+            self.end_date
+        )
+        reader.read()
+        reader_value = reader.get_data_values()
+        results = {}
+        for dt_idx, dt in enumerate(
+            reader_value.xr_dataset['date'].values):
+            timestamp = (
+                    (dt - np.datetime64('1970-01-01T00:00:00')) /
+                    np.timedelta64(1, 's')
+            )
+            dt = datetime.fromtimestamp(timestamp, tz=pytz.UTC)
+            month_day = dt.strftime('%m-%d')
+            data = {
+                'date': dt.strftime('%Y-%m-%d')
+            }
+            for var_name in self.ATTRIBUTES:
+                v = reader_value.xr_dataset[var_name].values[dt_idx]
+                var_map = self.VAR_MAPPING.get(var_name)
+                data[var_map] = (
+                    v if not np.isnan(v) else None
+                )
+            results[month_day] = data
+
+        return results
+
+    def clear_cache(self):
+        """Clear the cache for Tio zarr."""
+        cache.delete(self.CACHE_KEY_TIO_ZARR)
+
     def load_data(self):
         """Load the input data."""
-        historical_dict = self._fetch_timelines_data()
+        # Check if the date is in zarr
+        is_date_in_zarr = self._is_date_in_zarr(self.current_date)
+        if is_date_in_zarr:
+            historical_dict = self._read_forecast_from_zarr()
+        else:
+            historical_dict = self._fetch_timelines_data()
+
         final_dict = self._fetch_ltn_data(historical_dict)
         return final_dict
