608951905912ef135ee1efbf5e51f6e7415ad0bc
Danang Massandy <danangmassandy@gmail.com>
2025-06-19 01:14:01 +0000

    remove update all farms in grid ingestor; fix update progress; and use country_id config

--- django_project/gap/ingestor/grid.py
+++ django_project/gap/ingestor/grid.py
@@ -12,7 +12,7 @@ from gap.ingestor.base import BaseIngestor
 from gap.ingestor.exceptions import (
     FileNotFoundException, FileIsNotCorrectException
 )
-from gap.models import IngestorSession, Grid, Country, Farm
+from gap.models import IngestorSession, Grid, Country, IngestorSessionStatus
 
 HEADER_IDX = 0
 
@@ -51,8 +51,11 @@ class GridIngestor(BaseIngestor):
         df.reset_index(drop=True, inplace=True)
         data = df.to_dict(orient='records')
 
+        country_id = self.get_config('country_id')
+
         # Process the farm
         total = len(data)
+        progress = self._add_progress(f'Processing {total}')
         for idx, row in enumerate(data):
             try:
                 unique_id = row[Keys.UNIQUE_ID]
@@ -60,13 +63,16 @@ class GridIngestor(BaseIngestor):
                 shapewkt = row[Keys.SHAPEWKT]
                 geometry = WKTReader().read(shapewkt)
                 name = row[Keys.NAME]
+                defaults_dict = {
+                    'name': name,
+                    'elevation': elevation
+                }
+                if country_id:
+                    defaults_dict['country_id'] = country_id
                 grid, _ = Grid.objects.update_or_create(
                     unique_id=unique_id,
                     geometry=geometry,
-                    defaults={
-                        'name': name,
-                        'elevation': elevation
-                    }
+                    defaults=defaults_dict
                 )
                 if not grid.country:
                     try:
@@ -89,8 +95,15 @@ class GridIngestor(BaseIngestor):
                     f'Row {idx + HEADER_IDX + 2} : {e}'
                 )
 
-            self.session.notes = f'{idx + 1}/{total}'
-            self.session.save()
+            if idx % 100 == 0:
+                progress.notes = f'{idx + 1}/{total}'
+                progress.save()
+
+        progress.notes = f'{idx + 1}/{total}'
+        progress.status = IngestorSessionStatus.SUCCESS
+        progress.save()
+        self.session.notes = f'{idx + 1}/{total}'
+        self.session.save()
 
     def run(self):
         """Run the ingestor."""
@@ -102,7 +115,10 @@ class GridIngestor(BaseIngestor):
             self._run()
 
             # Assign farm to a grid
-            for farm in Farm.objects.all():
-                farm.assign_grid()
+            # NOTE:
+            # - use separate job
+            # - we don't need to iterate for all farms
+            # for farm in Farm.objects.all():
+            #     farm.assign_grid()
         except Exception as e:
             raise Exception(e)
