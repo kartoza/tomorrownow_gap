d72b09390da117054dc75e8500a4825a87058375
Danang <danangmassandy@gmail.com>
2025-07-09 12:46:23 +0100

    fix redis variables (#723)

--- job_polling/app/core/config.py
+++ job_polling/app/core/config.py
@@ -21,8 +21,8 @@ class Settings(BaseSettings):
 
     # Redis configuration
     REDIS_HOST: str = "localhost"
-    REDIS_PORT: int = 6379
-    REDIS_DB: int = 0
+    # REDIS_PORT: int = 6379
+    # REDIS_DB: int = 0
     REDIS_PASSWORD: str = ""
 
     # API configuration
