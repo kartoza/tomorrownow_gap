952bb3d911c17c52e2ce60a02deb28d48d5f9c42
Danang Massandy <danangmassandy@gmail.com>
2025-07-29 15:36:07 +0100

    fix lint

--- django_project/gap/management/commands/find_grid.py
+++ django_project/gap/management/commands/find_grid.py
@@ -1,29 +1,57 @@
+# coding=utf-8
+"""
+Tomorrow Now GAP.
+
+.. note:: Command to find grid.
+"""
+
 from django.core.management.base import BaseCommand, CommandError
 from django.db import models
+
 from gap.models import Grid
 
+
 class Command(BaseCommand):
+    """Command to find a Grid model instance by its unique id."""
     help = 'Finds a Grid model instance by its unique id'
 
     def add_arguments(self, parser):
-        parser.add_argument('grid_id', type=str, help='The unique id of the Grid')
+        """Add command line arguments."""
+        parser.add_argument(
+            'grid_id',
+            type=str,
+            help='The unique id of the Grid'
+        )
 
     def handle(self, *args, **options):
+        """Handle the command."""
         grid_id = options['grid_id']
         try:
             grid = Grid.objects.get(unique_id=grid_id)
             self.stdout.write(self.style.SUCCESS(f'Grid found: {grid}'))
             # print the centroid latitude and longitude
-            self.stdout.write(f'Centroid Latitude: {grid.geometry.centroid.y}')
-            self.stdout.write(f'Centroid Longitude: {grid.geometry.centroid.x}')
+            self.stdout.write(
+                f'Centroid Latitude: {grid.geometry.centroid.y}'
+            )
+            self.stdout.write(
+                f'Centroid Longitude: {grid.geometry.centroid.x}'
+            )
 
             # Grid Stats
             count = Grid.objects.count()
             self.stdout.write(f'Total Grids: {count}')
             # grid stats for each country
-            country_stats = Grid.objects.values('country__name').annotate(count=models.Count('id')).order_by('country__name')
+            country_stats = Grid.objects.values(
+                'country__name'
+            ).annotate(
+                count=models.Count('id')
+            ).order_by('country__name')
             for stat in country_stats:
-                self.stdout.write(f"Country: {stat['country__name']}, Grids: {stat['count']}")
-            self.stdout.write(self.style.SUCCESS('Grid stats retrieved successfully.'))
+                self.stdout.write(
+                    f"Country: {stat['country__name']}, Grids: {stat['count']}"
+                )
+            self.stdout.write(
+                self.style.SUCCESS('Grid stats retrieved successfully.')
+            )
         except Grid.DoesNotExist:
             raise CommandError(f'Grid with id {grid_id} does not exist.')
